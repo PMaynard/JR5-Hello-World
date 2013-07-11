d995873994781bff8e986b14d04d423508e259e7
SteveX <stevex@ultrajam.net>
2013-07-11 20:28:32 +0100

    Back in line after merge

--- include.php
+++ include.php
@@ -32,3 +32,18 @@ function jrHelloWorld_init()
 {
     return true;
 }
+
+/**
+ * magic view init
+ *
+ * Which would be seen for all profiles at a url something like:
+ * /stevex/audio/sayhellotome
+ * /stevex/video/sayhellotome
+ *
+ */
+
+// function jrHelloWorld_init()
+// {
+//     jrCore_register_module_feature('jrCore','magic_view','jrHelloWorld','sayhellotome','view_jrHelloWorld_hi');
+//     return true;
+// }
